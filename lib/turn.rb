574b61206248ad7f519f150096d2f2d43fcaf048
Sarah Peters <sarahpieters15@gmail.com>
2020-04-27 13:55:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,3 +11,11 @@ def input_to_index(user_input)
   new_user_input -= 1
   return new_user_input
 end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
